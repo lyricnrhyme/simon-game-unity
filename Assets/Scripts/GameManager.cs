4811a500cd489a890f54691ce08a49710bb02e02
lyricnrhyme <jamieyamashita.12@gmail.com>
2022-03-25 23:13:33 -1000

    Resolved bug where buttons would check for correct answer when game is not in session

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,174 +4,245 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
     List<int> userSeq = new List<int> { };
+
     public List<int> simonSeq = new List<int> { };
+
     int level = 0;
+
     int userCurrentStep = 0;
+
     int simonCurrentStep = 0;
+
     int highScore = 0;
+
     bool simonsTurn = false;
 
     // 5 seconds for the timer
     float timer = 5.0f;
+
     bool isGameBoardOn = false;
 
     public Text scoreDisplay;
+
     public Text highScoreDisplay;
+
     public GameObject[] pads;
+
     public GameObject onOffSwitch;
+
     Animator onOffSwitchAnimator;
 
     Coroutine timesUp;
+
     bool timerTriggered = false;
+
     bool timerStopped = false;
 
+    bool isGameInSession = false;
+
     // Start is called before the first frame update
-    void Start () {
-        onOffSwitchAnimator = onOffSwitch.GetComponent<Animator> ();
+    void Start()
+    {
+        onOffSwitchAnimator = onOffSwitch.GetComponent<Animator>();
     }
 
-    void Update() {
-        if (timerTriggered) {
+    void Update()
+    {
+        if (timerTriggered)
+        {
             if (timesUp != null) StopCoroutine(timesUp);
             timesUp = StartCoroutine(TimesUp());
             timerTriggered = false;
         }
 
-        if (timerStopped) {
+        if (timerStopped)
+        {
             if (timesUp != null) StopCoroutine(timesUp);
             timerStopped = false;
         }
     }
 
-    public void StartGame () {
-        if (isGameBoardOn) {
-            ResetGame ();
-            SimonSequence ();
+    public void StartGame()
+    {
+        if (isGameBoardOn)
+        {
+            ResetGame();
+            SimonSequence();
         }
     }
 
-    public void HandleOnOffSwitch () {
+    public void HandleOnOffSwitch()
+    {
         isGameBoardOn = !isGameBoardOn;
         scoreDisplay.text = isGameBoardOn ? "00" : "";
 
-        foreach (GameObject pad in pads) {
-            Pad padScript = pad.GetComponent<Pad> ();
-            if (isGameBoardOn) {
-                padScript.ChangePadColor (padScript.lightColor);
-            } else {
-                padScript.ChangePadColor (padScript.darkColor);
+        foreach (GameObject pad in pads)
+        {
+            Pad padScript = pad.GetComponent<Pad>();
+            if (isGameBoardOn)
+            {
+                padScript.ChangePadColor(padScript.lightColor);
+            }
+            else
+            {
+                padScript.ChangePadColor(padScript.darkColor);
             }
         }
 
-        if (isGameBoardOn) {
-            onOffSwitchAnimator.Play ("TurnOn");
-            highScoreDisplay.text = highScore >= 10 ? highScore.ToString () : "0" + highScore.ToString ();
-        } else {
-            ResetGame ();
-            onOffSwitchAnimator.Play ("TurnOff");
+        if (isGameBoardOn)
+        {
+            onOffSwitchAnimator.Play("TurnOn");
+            highScoreDisplay.text =
+                highScore >= 10
+                    ? highScore.ToString()
+                    : "0" + highScore.ToString();
+        }
+        else
+        {
+            ResetGame();
+            onOffSwitchAnimator.Play("TurnOff");
             highScoreDisplay.text = "";
+            timerStopped = true;
+            isGameInSession = false;
         }
     }
 
-    void DisplayError () {
-        scoreDisplay.text = "XX";
-        foreach (GameObject pad in pads) {
-            Pad padScript = pad.GetComponent<Pad> ();
-            padScript.PlaySound ();
-        }
+    void DisplayError()
+    {
+        if (isGameBoardOn)
+        {
+            scoreDisplay.text = "XX";
+            isGameInSession = false;
+            foreach (GameObject pad in pads)
+            {
+                Pad padScript = pad.GetComponent<Pad>();
+                padScript.PlaySound();
+            }
 
-        if (level > highScore) {
-            highScore = level;
-            highScoreDisplay.text = highScore >= 10 ? highScore.ToString () : "0" + highScore.ToString ();
-        }
-        ResetGame ();
+            if (level > highScore)
+            {
+                highScore = level;
+                highScoreDisplay.text =
+                    highScore >= 10
+                        ? highScore.ToString()
+                        : "0" + highScore.ToString();
+            }
+            ResetGame();
 
-        StartCoroutine (ResetScoreDisplay ());
+            StartCoroutine(ResetScoreDisplay());
+        }
     }
 
-    void SimonSequence () {
-        if (isGameBoardOn) {
+    void SimonSequence()
+    {
+        if (isGameBoardOn)
+        {
+            isGameInSession = true;
             simonsTurn = true;
-            scoreDisplay.text = level >= 10 ? level.ToString () : "0" + level.ToString ();
-            GetRandomNum ();
+            scoreDisplay.text =
+                level >= 10 ? level.ToString() : "0" + level.ToString();
+            GetRandomNum();
             StartCoroutine(PlaySimonSequence());
-            simonsTurn = false;
             timerTriggered = true;
-        } else {
+        }
+        else
+        {
             simonsTurn = false;
+            isGameInSession = false;
         }
     }
 
-    public void CheckSimonSequence (GameObject pad) {
-        if (!simonsTurn && isGameBoardOn) {
-            Pad padScript = pad.GetComponent<Pad> ();
-            userSeq.Add (padScript.id);
-            padScript.AddClassSound ();
+    public void CheckSimonSequence(GameObject pad)
+    {
+        if (!simonsTurn && isGameBoardOn)
+        {
+            Pad padScript = pad.GetComponent<Pad>();
+            userSeq.Add(padScript.id);
+            padScript.AddClassSound();
             timerStopped = true;
 
-            if (padScript.id != simonSeq[userCurrentStep]) {
-                DisplayError ();
-            } else {
-                timerTriggered = true;
-                userCurrentStep++;
+            if (isGameInSession)
+            {
+                if (padScript.id != simonSeq[userCurrentStep])
+                {
+                    DisplayError();
+                }
+                else
+                {
+                    timerTriggered = true;
+                    userCurrentStep++;
 
-                if (userSeq.Count == simonSeq.Count) {
-                    level++;
-                    StartCoroutine(NextLevelSimonSequence());
+                    if (userSeq.Count == simonSeq.Count)
+                    {
+                        level++;
+                        StartCoroutine(NextLevelSimonSequence());
+                    }
                 }
             }
         }
     }
 
-    void ResetGame () {
+    void ResetGame()
+    {
+        isGameInSession = false;
         level = 0;
-        simonSeq.Clear ();
-        ResetUserInput ();
+        simonSeq.Clear();
+        ResetUserInput();
         timerStopped = true;
     }
 
-    void ResetUserInput () {
+    void ResetUserInput()
+    {
         userCurrentStep = 0;
-        userSeq.Clear ();
+        userSeq.Clear();
     }
 
-    void GetRandomNum () {
-        int rInt = UnityEngine.Random.Range (0, 4);
+    void GetRandomNum()
+    {
+        int rInt = UnityEngine.Random.Range(0, 4);
         simonSeq.Add (rInt);
     }
 
-    IEnumerator TimesUp () {
-        yield return new WaitForSeconds (timer);
-        DisplayError ();
+    IEnumerator TimesUp()
+    {
+        yield return new WaitForSeconds(timer);
+        DisplayError();
     }
 
-    IEnumerator NextLevelSimonSequence() {
+    IEnumerator NextLevelSimonSequence()
+    {
+        simonsTurn = true;
         yield return new WaitForSeconds(1.0f);
         ResetUserInput();
         SimonSequence();
         timerStopped = true;
     }
 
-    IEnumerator PlaySimonSequence () {
+    IEnumerator PlaySimonSequence()
+    {
         int colorValue = simonSeq[simonCurrentStep];
-        pads[colorValue].GetComponent<Pad> ().AddClassSound ();
+        pads[colorValue].GetComponent<Pad>().AddClassSound();
         simonCurrentStep++;
-        yield return new WaitForSeconds (1.0f);
-        if (simonCurrentStep >= simonSeq.Count) {
+        yield return new WaitForSeconds(1.0f);
+        if (simonCurrentStep >= simonSeq.Count)
+        {
             simonsTurn = false;
             simonCurrentStep = 0;
-            StopCoroutine (PlaySimonSequence ());
+            StopCoroutine(PlaySimonSequence());
             timerTriggered = true;
-        } else {
-            StartCoroutine (PlaySimonSequence ());
+        }
+        else
+        {
+            StartCoroutine(PlaySimonSequence());
         }
     }
 
-    IEnumerator ResetScoreDisplay () {
-        yield return new WaitForSeconds (1.5f);
+    IEnumerator ResetScoreDisplay()
+    {
+        yield return new WaitForSeconds(1.5f);
         scoreDisplay.text = "00";
     }
-
 }
